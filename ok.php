440c30ae042a0e99bf74aeab5c68b482276b3378
Kai Großmann <soranota@gmail.com>
2013-10-24 23:20:13 +0200

    3.

--- ok.php
+++ ok.php
@@ -3,13 +3,5 @@
 <?php
 
 
- $to = "soranota@gmail.com";
- $subject = "Hi!";
- $body = "OK";
- if (mail($to, $subject, $body)) {
-   echo("<p>Email successfully sent!</p>");
-  } else {
-   echo("<p>Email delivery failed…</p>");
-  }
 
 ?>
